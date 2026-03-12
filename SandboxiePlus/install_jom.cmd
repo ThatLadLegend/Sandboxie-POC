125d26f1e0aa246c8576f30c60216217de549e3e
Legend <legend@thatladlegend.dev>
2026-03-12 01:32:28 +0000

    Update install_jom.cmd

--- SandboxiePlus/install_jom.cmd
+++ SandboxiePlus/install_jom.cmd
@@ -2,7 +2,7 @@ if exist "%~dp0..\..\Qt\Tools\QtCreator\bin\jom.exe" goto done
 
 curl -L -o "%~dp0..\..\jom_1_1_4.zip" https://download.qt.io/official_releases/jom/jom_1_1_4.zip
 
-"E:\Program Files\7-Zip\7z.exe" x -aoa -o"%~dp0..\..\Qt\Tools\QtCreator\bin\" "%~dp0..\..\jom_1_1_4.zip"
+"C:\Program Files\7-Zip\7z.exe" x -aoa -o"%~dp0..\..\Qt\Tools\QtCreator\bin\" "%~dp0..\..\jom_1_1_4.zip"
 
 :done
 
